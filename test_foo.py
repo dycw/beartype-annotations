b3c10f72456ae45136a87fde342b68f293b350f0
Derek Wan <d.wan@icloud.com>
2021-11-20 14:45:22 +0800

    Found the minimal example

--- test_foo.py
+++ test_foo.py
@@ -4,9 +4,10 @@ from beartype import beartype
 
 
 @beartype
-def foo(x: int) -> int:
-    return x + 1
+def foo(x: int | None) -> int:
+    return 0 if x is None else (x + 1)
 
 
 def test_main():
+    assert foo(None) == 2
     assert foo(1) == 2
